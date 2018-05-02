ba6b3365cb0bd28efb3b6a78105ab4c131da06df
Hugo Silva <jhssilva96@gmail.com>
2018-05-02 19:54:55 +0100

    rwr
    wrw

--- 9 Card Siege/src/Main.java
+++ 9 Card Siege/src/Main.java
@@ -3,10 +3,10 @@ import logicaJogo.Jogo;
 class Main {
     public static void main(String[] args) {
 
-        Jogo jogo = new Jogo();
+            Jogo jogo = new Jogo();
 
-        jogo.start();
+            jogo.start();
 
-        System.out.println("Fim!\n");
+            System.out.println("Fim!\n");
     }
 }
