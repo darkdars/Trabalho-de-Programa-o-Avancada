830d5905c5b9b26929c26e64b2ae1a615d126e3d
S0RA69 <34314037+S0RA69@users.noreply.github.com>
2018-05-08 23:23:01 +0100

    Cartas extend

---
+++ 9GameSiege/9GameSiege/src/logicaJogo/Cartas/Card.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicaJogo.Cartas;
+
+import java.util.List;
+
+/**
+ *
+ * @author Andre
+ */
+public class Card {
+    private List<Eventos> eventos;
+    
+    Card(){
+        eventos = new List<Eventos>() {};
+    }
+}
