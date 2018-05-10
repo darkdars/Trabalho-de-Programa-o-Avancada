9826b74a4f6089597e54ec454bd6b59ac88f4b2e
Hugo Silva <jhssilva96@gmail.com>
2018-05-10 23:56:25 +0100

    Ei
    dadada

--- 9GameSiege/9GameSiege/src/logicaJogo/estados/AwaitEnemyMovementPhase.java
+++ 9GameSiege/9GameSiege/src/logicaJogo/estados/AwaitEnemyMovementPhase.java
@@ -23,53 +23,66 @@ public class AwaitEnemyMovementPhase extends EstadoAdapter{
 
          //Primeiro Obter evento depois dar handle
          
-         for(int i = 0; i < dadosJogo.getEventoAtual().getEnemySize();i++){
-             
-             if(dadosJogo.getEventoAtual().getEnemyString(i) == "sword"){
-                 //Obter unidades mais lentas
-                 
-                if((dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() == dadosJogo.getEnemyTracks().getAriete())){
-                    dadosJogo.getEnemyTracks().updateEscada(-1);
-                    dadosJogo.getEnemyTracks().updateAriete(-1);
-                    dadosJogo.getEnemyTracks().updateTorre(-1);    
-                }else if((dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() != dadosJogo.getEnemyTracks().getAriete())){
-                    dadosJogo.getEnemyTracks().updateAriete(-1);
-                    dadosJogo.getEnemyTracks().updateEscada(-1);   
-                }else if((dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() != dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() == dadosJogo.getEnemyTracks().getAriete())){
-                    dadosJogo.getEnemyTracks().updateAriete(-1);
-                    dadosJogo.getEnemyTracks().updateTorre(-1);    
-                }else if((dadosJogo.getEnemyTracks().getEscada() != dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() == dadosJogo.getEnemyTracks().getAriete())){
-                    dadosJogo.getEnemyTracks().updateEscada(-1);
-                    dadosJogo.getEnemyTracks().updateTorre(-1);    
-                }else if((dadosJogo.getEnemyTracks().getEscada() >= dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() >= dadosJogo.getEnemyTracks().getAriete())){
-                    dadosJogo.getEnemyTracks().updateEscada(-1);    
-                }else if((dadosJogo.getEnemyTracks().getEscada() <= dadosJogo.getEnemyTracks().getTorre() && dadosJogo.getEnemyTracks().getTorre() >= dadosJogo.getEnemyTracks().getAriete())){
-                    dadosJogo.getEnemyTracks().updateTorre(-1);    
-                }else if((dadosJogo.getEnemyTracks().getEscada() <= dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() <= dadosJogo.getEnemyTracks().getAriete())){
-                    dadosJogo.getEnemyTracks().updateAriete(-1);    
-                }
-                 
-                 
-                 
-                 
-                 
-             }else if(dadosJogo.getEventoAtual().getEnemyString(i) == "tower"){
-                 
-             }else if(dadosJogo.getEventoAtual().getEnemyString(i) == "ladder"){
-                 
-             }else if(dadosJogo.getEventoAtual().getEnemyString(i) == "ram"){
-                 
-             }else if(dadosJogo.getEventoAtual().getEnemyString(i) == "none"){
+         if(dadosJogo.getEventoAtual().getEnemySize() > 1){
+                
+             for(int i = 0; i < dadosJogo.getEventoAtual().getEnemySize();i++){
                  
              }
+            
              
+                
+                
+         }else{
+             if(dadosJogo.getEventoAtual().getEnemyString(0) == "sword"){
+                 //Obter unidades mais lentas
+                 for(int i = 0; i < 3;i++){
+               
+                     if((dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() == dadosJogo.getEnemyTracks().getAriete())){
+                        dadosJogo.getEnemyTracks().updateEscada(-1);
+                        dadosJogo.getEnemyTracks().updateAriete(-1);
+                        dadosJogo.getEnemyTracks().updateTorre(-1);    
+                    }else if((dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() != dadosJogo.getEnemyTracks().getAriete())){
+                        dadosJogo.getEnemyTracks().updateAriete(-1);
+                        dadosJogo.getEnemyTracks().updateEscada(-1);   
+                    }else if((dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() != dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() == dadosJogo.getEnemyTracks().getAriete())){
+                        dadosJogo.getEnemyTracks().updateAriete(-1);
+                        dadosJogo.getEnemyTracks().updateTorre(-1);    
+                    }else if((dadosJogo.getEnemyTracks().getEscada() != dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() == dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() == dadosJogo.getEnemyTracks().getAriete())){
+                        dadosJogo.getEnemyTracks().updateEscada(-1);
+                        dadosJogo.getEnemyTracks().updateTorre(-1);    
+                    }else if((dadosJogo.getEnemyTracks().getEscada() >= dadosJogo.getEnemyTracks().getTorre() &&  dadosJogo.getEnemyTracks().getEscada() >= dadosJogo.getEnemyTracks().getAriete())){
+                        dadosJogo.getEnemyTracks().updateEscada(-1);    
+                    }else if((dadosJogo.getEnemyTracks().getEscada() <= dadosJogo.getEnemyTracks().getTorre() && dadosJogo.getEnemyTracks().getTorre() >= dadosJogo.getEnemyTracks().getAriete())){
+                        dadosJogo.getEnemyTracks().updateTorre(-1);    
+                    }else if((dadosJogo.getEnemyTracks().getEscada() <= dadosJogo.getEnemyTracks().getAriete() && dadosJogo.getEnemyTracks().getTorre() <= dadosJogo.getEnemyTracks().getAriete())){
+                        dadosJogo.getEnemyTracks().updateAriete(-1);    
+                    }
+                 }
+                              
+             }else if(dadosJogo.getEventoAtual().getEnemyString(0) == "tower"){
+                 dadosJogo.getEnemyTracks().updateTorre(-1);
+             }else if(dadosJogo.getEventoAtual().getEnemyString(0) == "ladder"){
+                 dadosJogo.getEnemyTracks().updateEscada(-1);
+             }else if(dadosJogo.getEventoAtual().getEnemyString(0) == "ram"){
+                 dadosJogo.getEnemyTracks().updateAriete(-1);
+             }else if(dadosJogo.getEventoAtual().getEnemyString(0) == "none"){
+                 return new AwaitPlayerAction(dadosJogo);
+             }
              
-         }
+             }
+             
+         
+         
+         
+
          
          
          
          
+     
          return this;
-     }
+     
     
+}
+     
 }
