b7f2c24d1c0e3aa313531020f9ed09e0a66d7b1e
Hugo Silva <jhssilva96@gmail.com>
2018-05-13 11:33:24 +0100

    End of Day Phase Quase Feito
    Falta fazer a 3 opção que é das enemy line space.

--- 9GameSiege/9GameSiege/src/logicaJogo/estados/AwaitEndDayPhase.java
+++ 9GameSiege/9GameSiege/src/logicaJogo/estados/AwaitEndDayPhase.java
@@ -17,4 +17,44 @@ public class AwaitEndDayPhase extends EstadoAdapter{
         super(dadosJogo);
     }
     
+    @Override
+    public IEstado endDayPhase(){
+        
+        //Check if we win
+        if(dadosJogo.getDia() > 3){
+            dadosJogo.setTexto("Parabens!!!! Sobreviveu aos ataques do inimigo!");
+            return this;
+        }
+        
+        //Suplies reduction
+        dadosJogo.getStatusCard().setMantimentos(dadosJogo.getStatusCard().getMantimentos() - 1);
+        
+        //soldiers back to the tunnel
+        if(dadosJogo.getStatusCard().getTunel() == 2 || dadosJogo.getStatusCard().getTunel() == 3){
+            dadosJogo.getStatusCard().setTunel(1);
+            
+            if(dadosJogo.getStatusCard().getMantimentos() != 4){
+             
+            if(dadosJogo.getStatusCard().getMantimentos() == 3){
+                if(dadosJogo.getStatusCard().getMantimentosRoubados() == 2){
+                    dadosJogo.getStatusCard().setMantimentos(dadosJogo.getStatusCard().getMantimentos() + 1);
+                }else{
+                    dadosJogo.getStatusCard().setMantimentos(dadosJogo.getStatusCard().getMantimentos() + dadosJogo.getStatusCard().getMantimentosRoubados());
+                }
+            }else{
+                 dadosJogo.getStatusCard().setMantimentos(dadosJogo.getStatusCard().getMantimentos() + dadosJogo.getStatusCard().getMantimentosRoubados());
+            }    
+                
+            }
+            
+            dadosJogo.getStatusCard().setMantimentosRoubados(0);
+        }
+        
+        // If are enemys on enemy line space capture
+       // if(dadosJogo.getEnemyTracks())
+        
+        
+        return new AwaitLineCheck(dadosJogo);
+    }
+    
 }
